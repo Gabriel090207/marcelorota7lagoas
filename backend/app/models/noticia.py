dd184a07fbb1ccc342d5a31419a18bfb3bf7585e
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-04-01 17:07:13 -0300

    salva foto imagens

--- backend/app/models/noticia.py
+++ backend/app/models/noticia.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from datetime import datetime
+
 
 class Noticia(BaseModel):
     titulo: str
     conteudo: str
     imagem: Optional[str] = None
+    imagens: Optional[List[str]] = []  # 🔥 ESSA LINHA RESOLVE
     categoria: Optional[str] = None
     autor: Optional[str] = "Admin"
     data: Optional[datetime] = datetime.now()
